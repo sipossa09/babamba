f7f85f53cbd0f5b86e3e32dc0432ea5c7d02bc7f
sipossa09 <90168520+sipossa09@users.noreply.github.com>
2021-12-10 15:17:24 +0900

    20211210

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# babamba
- babamba good
+학원에서 배운 거 백업합니다.
---
+++ doc/float02.js
@@ -0,0 +1,39 @@
+$(function(){
+    $('.main_slider').slick({
+        arrows:false,
+    });
+});
+
+
+
+
+// window.onload=function(){
+
+// }
+
+// window.addEventListener('DOMCOntentLoaded', function(){
+    
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
